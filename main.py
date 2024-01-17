d8afb3093c775440b6056450547ec615c096ad7c
Paul Davis <pauldavis101@gmail.com>
2024-01-16 19:23:41 -0600

    Update main.py
    Support for MC 1.20.4, or whenever it changed to "Shapes.or" and "Block.box"

--- main.py
+++ main.py
@@ -1,30 +1,34 @@
 import json
 import sys
 
-
 if len(sys.argv) < 2:
-	print("Usage: " + sys.argv[0] + " <INPUT>")
-	exit()
+    print("Usage: " + sys.argv[0] + " <INPUT>")
+    exit()
 
 INPUT = sys.argv[1]
 
 print("Reading: {}...".format(INPUT))
 
 with open(INPUT) as f:
-	data = json.load(f)
+    data = json.load(f)
 
 cubes = data["elements"]
 
 output = []
-output.append("public static VoxelShape SHAPE = VoxelShapes.or(")
+output.append("public static VoxelShape SHAPE = Shapes.or(")
 
-for cube in cubes:
-	ln = "\tBlock.makeCuboidShape({}, {}, {}, {}, {}, {}),"
-	x1, y1, z1 = cube["from"]
-	x2, y2, z2 = cube["to"]
-	output.append(ln.format(x1, y1, z1, x2, y2, z2))
+for index, cube in enumerate(cubes):
+    if index == len(cubes) - 1:
+        ln = "\tBlock.box({}, {}, {}, {}, {}, {}));"
+        x1, y1, z1 = cube["from"]
+        x2, y2, z2 = cube["to"]
+        output.append(ln.format(x1, y1, z1, x2, y2, z2))
+    else:
+        ln = "\tBlock.box({}, {}, {}, {}, {}, {}),"
+        x1, y1, z1 = cube["from"]
+        x2, y2, z2 = cube["to"]
+        output.append(ln.format(x1, y1, z1, x2, y2, z2))
 
-output.append(");")
 
 result = "\n".join(output)
 
